4b66f74455556954a4b81c546c8d77f14058f203
FoozBarakat <56113235+FoozBarakat@users.noreply.github.com>
2019-11-04 10:17:04 +0100

    Challenge_4

--- main.js
+++ main.js
@@ -7,8 +7,18 @@
 //The function should never return undefined
 
 function operateOn(firstNumber, secondNumber, operation) {
-  //TODO: your code here
+  if (operation === 0) {
+    return firstNumber + secondNumber;
+  } else if (operation === 1) {
+    return firstNumber * secondNumber;
+  } else if (operation === 2) {
+    return firstNumber / secondNumber;
+  } else {
+    return'Nothing to Operate On';
+  }
 }
+
+console.log( operateOn(2,3,0) );
 
 //========================================================== 2
 //Write a function called addArrays that takes two arrays as parameters (firstArray, secondArray)
@@ -17,18 +27,80 @@ function operateOn(firstNumber, secondNumber, operation) {
 //Write the function using while loop and for loop
 
 function addArraysWhileLoop(firstArray, secondArray) {
-  //TODO: your code here
+  var result = [];
+  var i = 0;
+  var j = 0;
+  var len;
+  
+  if (firstArray.length > secondArray.length) {
+    len = firstArray.length;
+  } else {
+    len = secondArray.length;
+  }
+
+  while (i < len) {
+    if (firstArray[i] === undefined) {
+        result.push(secondArray[i] + 1)
+      } else if (secondArray[i] === undefined) {
+        result.push(firstArray[i] + 1);
+      } else {
+        result.push(firstArray[i] + secondArray[i]);   
+      }
+
+    i++;
+  }
+
+  return result;
 }
 
+console.log( addArraysWhileLoop([1], [1,2]) );
+
 function addArraysForLoop(firstArray, secondArray) {
-  //TODO: you code here
+  var result = [];
+  var len;
+
+   if (firstArray.length > secondArray.length) {
+    len = firstArray.length;
+  } else {
+    len = secondArray.length;
+  }
+
+  for (var i = 0; i < len; i++) {
+    if (firstArray[i] === undefined) {
+        result.push(secondArray[i] + 1)
+      } else if (secondArray[i] === undefined) {
+        result.push(firstArray[i] + 1);
+      } else {
+        result.push(firstArray[i] + secondArray[i]);   
+      }
+  }
+
+  return result;
 }
+
+console.log( addArraysForLoop([2,4,1], [1,1]));
 
 //=========================================================== 3
 //Using recursion, return the sum of all of the positives numbers of an array of numbers.
 // posSum[1,-4,7,12] => 1 + 7 + 12 = 20
 
-//TODO: you code here
+function sumPositives(numbers, result, i) {
+  var result = result || 0;
+  var i = i || 0;
+
+  if (i === numbers.length) {
+    return result;
+  }
+
+  if (numbers[i] > 0) {
+  result += numbers[i];
+  }
+
+  return sumPositives(numbers, result, ++i);
+}
+
+console.log('hi')
+console.log( sumPositives( [1,-4,7,12] ) );
 
 //=========================================================== 4
 //I have a bucket of sloths. Each sloth is special and has a long name.
@@ -45,8 +117,18 @@ var bucketOfSloths = [
 // fullName(bucketOfSloths,0) //==> "Furry Danger Assassin"
 
 function fullName(array, index) {
-  //your code is here
+  var result = '';
+
+  for (var key1 in array[index]) {
+    for (var key2 in array[index][key1]) {
+    result += " " + array[index][key1][key2];
+  }
+  }
+
+  return result;
 }
+
+console.log( fullName(bucketOfSloths, 0) );
 
 //  b- Write a function that takes an array of objects (like bucketOfSloths)
 //  and returns the sloth (should return an object)
@@ -56,7 +138,23 @@ function fullName(array, index) {
 // => {name: {first: "Furry", middle: "Danger", last: "Assassin"}, age: 2}
 
 function longestName(bucketOfSloths) {
-  // TODO: Your code here
+  var result = {};
+  var long = fullName(bucketOfSloths, 0);
+  var index = 0;
+  var name; 
+
+  for (var i = 0; i < bucketOfSloths.length; i++) {
+    name = fullName(bucketOfSloths, i);   
+    if (long.length < name.length ) {
+      long = name;
+      index = i;
+    }     
+  }
+
+  result = bucketOfSloths[index];
+  return result;
 }
+
+console.log( longestName(bucketOfSloths) );
 
 //Good Luck :))
